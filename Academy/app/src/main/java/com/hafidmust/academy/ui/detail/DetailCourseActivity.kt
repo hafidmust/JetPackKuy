f32766fc887e1cef49bcd7254cf932c11e905f5f
Hafid Ali Mustaqim <hafidalimustaqim13@gmail.com>
2021-12-12 12:32:15 +0700

    layouting project

--- Academy/app/src/main/java/com/hafidmust/academy/ui/detail/DetailCourseActivity.kt
+++ Academy/app/src/main/java/com/hafidmust/academy/ui/detail/DetailCourseActivity.kt
@@ -12,11 +12,8 @@ import com.hafidmust.academy.databinding.ActivityDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
-    companion object{
-        const val EXTRA_COURSE = "extra_course"
-    }
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+
     private lateinit var binding: ActivityDetailCourseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,19 +25,8 @@ class DetailCourseActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
     }
 }
