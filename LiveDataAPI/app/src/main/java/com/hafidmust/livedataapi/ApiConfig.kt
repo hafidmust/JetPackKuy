978c2b136d1fd380bbdaf7cc2afeb8cb600fae0e
Hafid Ali Mustaqim <hafidalimustaqim13@gmail.com>
2021-12-23 11:22:25 +0700

    livedata & retrofit

---
+++ LiveDataAPI/app/src/main/java/com/hafidmust/livedataapi/ApiConfig.kt
@@ -0,0 +1,26 @@
+package com.hafidmust.livedataapi
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class ApiConfig {
+    companion object{
+        fun getApiService() : ApiService{
+            val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://restaurant-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
--- LiveDataAPI/app/src/main/java/com/hafidmust/livedataapi/MainActivity.kt
+++ LiveDataAPI/app/src/main/java/com/hafidmust/livedataapi/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.hafidmust.livedataapi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.hafidmust.livedataapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        viewModel.restaurant.observe(this,{restaurant->
+            binding.tvTitle.text = restaurant.name
+            binding.tvDescription.text = restaurant.description
+            Glide.with(this)
+                .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                .into(binding.ivPicture)
+        })
+
+        viewModel.listReview.observe(this,{ consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+
+            binding.lvReview.adapter = ArrayAdapter(this, R.layout.item_review, listReview)
+            binding.edReview.setText("")
+        })
+
+        viewModel.isLoading.observe(this,{
+            binding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        binding.btnSend.setOnClickListener {
+            viewModel.postReview(binding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 }
